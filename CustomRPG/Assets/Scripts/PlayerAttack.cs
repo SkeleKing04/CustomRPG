c62679e24a3ab318c4855cf92351722604d334fd
Jackson Lucas <jjlucas04@bigpond.com>
2021-06-04 12:56:29 +0930

    Worked on player attack

--- CustomRPG/Assets/Scripts/PlayerAttack.cs
+++ CustomRPG/Assets/Scripts/PlayerAttack.cs
@@ -6,49 +6,70 @@ public class PlayerAttack : MonoBehaviour
 {
     CharacterInfoManager CharacterInfo;
     CharacterReader CharacterReader;
+    GameManager gameManager;
     public Transform fireTransform;
     private float damage;
     public Rigidbody projectile;
     public float projectileSpeed;
     //public effectSO effect;
     public static bool attacking = false;
+    public Component meleeScript;
+    public Component physicsRangedScript;
+    public Component rangedScript;
+    public Component boostScript;
+    public Component summonScript;
+
     // Start is called before the first frame update
     void Start()
     {
         CharacterInfo = FindObjectOfType<CharacterInfoManager>();
         CharacterReader = FindObjectOfType<CharacterReader>();
+        gameManager = FindObjectOfType<GameManager>();
+        meleeScript = GetComponent<Melee>();
+        physicsRangedScript = GetComponent<physicsRanged>();
+        rangedScript = GetComponent<Ranged>();
+        boostScript = GetComponent<Boost>();
+        summonScript = GetComponent<Summon>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyUp(KeyCode.Mouse0) && attacking == false)
+        if (gameManager.gameState == GameManager.e_GameState.Playing)
         {
-            damage = CharacterReader.m_Move1.damage;
-            projectile = CharacterReader.m_Move1.projectile;
-            projectile = CharacterReader.m_Move1.projectile;
-            CastAttack();
-        }
-        if (Input.GetKeyUp(KeyCode.Mouse1) && attacking == false)
-        {
-            damage = CharacterReader.m_Move2.damage;
-            projectile = CharacterReader.m_Move2.projectile;
-            projectile = CharacterReader.m_Move2.projectile;
-            CastAttack();
-        }
-        if (Input.GetKeyUp(KeyCode.Q) && attacking == false)
-        {
-            damage = CharacterReader.m_Move3.damage;
-            projectile = CharacterReader.m_Move3.projectile;
-            projectile = CharacterReader.m_Move3.projectile;
-            CastAttack();
-        }
-        if (Input.GetKeyUp(KeyCode.E) && attacking == false)
-        {
-            damage = CharacterReader.m_Move4.damage;
-            projectile = CharacterReader.m_Move4.projectile;
-            projectile = CharacterReader.m_Move4.projectile;
-            CastAttack();
+            if (Input.GetKeyUp(KeyCode.Mouse0) && attacking == false)
+            {
+                meleeScript.gameObject.SetActive(true);
+                physicsRangedScript.gameObject.SetActive(true);
+                rangedScript.gameObject.SetActive(true);
+                boostScript.gameObject.SetActive(true);
+                summonScript.gameObject.SetActive(true);
+                //damage = CharacterReader.m_Move1.damage;
+                projectile = CharacterReader.m_Move1.projectile;
+                projectile = CharacterReader.m_Move1.projectile;
+                CastAttack();
+            }
+            if (Input.GetKeyUp(KeyCode.Mouse1) && attacking == false)
+            {
+                damage = CharacterReader.m_Move2.damage;
+                projectile = CharacterReader.m_Move2.projectile;
+                projectile = CharacterReader.m_Move2.projectile;
+                CastAttack();
+            }
+            if (Input.GetKeyUp(KeyCode.Q) && attacking == false)
+            {
+                damage = CharacterReader.m_Move3.damage;
+                projectile = CharacterReader.m_Move3.projectile;
+                projectile = CharacterReader.m_Move3.projectile;
+                CastAttack();
+            }
+            if (Input.GetKeyUp(KeyCode.E) && attacking == false)
+            {
+                damage = CharacterReader.m_Move4.damage;
+                projectile = CharacterReader.m_Move4.projectile;
+                projectile = CharacterReader.m_Move4.projectile;
+                CastAttack();
+            }
         }
     }
     public void CastAttack()
